49414bd962d13c6195881b45dcd34928b789e570
Mairin Bailey <mairin.bailey@gmail.com>
2017-06-22 12:39:45 -0600

    add odd and even component functionality

--- src/app/even/even.component.ts
+++ src/app/even/even.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-even',
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class EvenComponent implements OnInit {
 
   constructor() { }
-
+  @Input() number: number;
   ngOnInit() {
   }
 
